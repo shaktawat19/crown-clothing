88ac47fb60b7955e76b190f56272e5b4123ae767
shaktawat19 <harshshaktawat.tech@gmail.com>
2023-07-02 12:43:27 +0530

    Home page & shop page design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </Router>
   );
 }
 
